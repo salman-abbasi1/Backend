e3842bb1df30ee5f29921065b3ab48b5d842ce01
salman-abbasi1 <salmanxop12@gmail.com>
2026-01-21 04:01:18 -0800

    inheritance

---
+++ es6 js/inheritance.js
@@ -0,0 +1,18 @@
+class Car{
+drive(){
+    document.write('This is Drive Method <br> <br>')
+ }
+stop(){
+document.write('This is Stop Method <br> <br>')
+}
+}
+
+class electicCar extends Car{
+    drive(){
+        super.drive()
+        document.write('This is drive method of electric car  <br>') //method overiding & polymorphism
+    }
+}
+let tesla = new electicCar()
+tesla.drive()
+tesla.stop()
