a22e1fa7055e9c701b8ee540da9fcf543387a7ae
ahsan@gmail.com <mahahanur019@gmail.com>
2026-02-02 19:12:29 +0500

    chngs

--- es6 js/regex.js
+++ es6 js/regex.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
+     // name
+    $('#name').keyup(function(){
+        let name = $(this).val()
+        let namereg = /^[a-zA-Z\s]{2,50}$/
+        if(!namereg.test(name)){
+            $(this).next('span').show().text('Only letters and spaces are allowed').css('color' , 'red')
+            $(this).css('border' , '2px solid red')
+        }
+        else{
+            $(this).next('span').hide()
+            $(this).css('border' , '2px solid green')
+        }
+    })
 
-    // name
+    // email
     $('#email').keyup(function(){
         let email = $(this).val()
-        let emailreg = /^[a-z0-9\-\_\.]{5,}[@][a-z]{5}[.]{a-z}{3} $/
+        let emailreg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
         if(!emailreg.test(email)){
-            $(this).next('span').show().text('Incorrect email').css('color' , 'red')
-            
+            $(this).next('span').show().text('Email must have @gmail.com').css('color' , 'red')
+            $(this).css('border' , '2px solid red')
         }
         else{
             $(this).next('span').hide()
